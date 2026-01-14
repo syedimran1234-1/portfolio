976cebd244ca41e611f36f3fce3f13117cb43477
syedimran1234-1 <syedimran1234.dba5@gmail.com>
2026-01-14 06:46:48 -0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom";
 import './index.css'
-import { AppProvider } from './context/context.jsx';
+import { AppProvider } from './context/Context.jsx';
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
